57a0f005c605380176c4a25961aa7c13c080f535
Saikumar Ketireddi <kethireddysaikumar7@gmail.com>
2024-04-30 11:59:42 +0530

    adding some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[]args){
+        Product p = new Product();
+        int ans = p.product(1,2);
+        int ans1 = p.product(3,4,6);
+        double ans2 = p.product(2.0,3.6);
+        System.out.println(ans+" "+ans1+" "+ans2);
 
+    }
+    //class
+    static class Product{
+        //method 1
+        //same method but different parameters
+        public int product(int x, int y) {
+            return x*y;
+        }
+        //method 2
+        public int product(int x, int y, int z) {
+            return x*y;
+        }
+        //method 3
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
